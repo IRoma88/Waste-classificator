0405a26bb4164cf3969731d9e8f1cc108aa58fb4
IRoma88 <ivan.romana88@gmail.com>
2025-09-07 19:43:37 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,21 +3,13 @@ import numpy as np
 from PIL import Image
 
 def load_models():
-    """
-    Carga los modelos completos desde los .h5
-    modelo_A.h5 -> Modelo principal
-    modelo_B_finetuned.h5 -> Modelo para subcategorías (SPECIAL)
-    """
-    model_A = tf.keras.models.load_model("modelo_A.h5", compile=False)
-    model_B = tf.keras.models.load_model("modelo_B_finetuned.h5", compile=False)
+    model_A = tf.keras.models.load_model("modelo_A.keras", compile=False)
+    model_B = tf.keras.models.load_model("modelo_B_finetuned.keras", compile=False)
     return model_A, model_B
 
 def preprocess_image(uploaded_file, target_size):
-    """
-    Preprocesa la imagen para que tenga el tamaño que espera el modelo.
-    """
     img = Image.open(uploaded_file).convert("RGB")
     img_resized = img.resize(target_size)
     img_array = tf.keras.utils.img_to_array(img_resized)
-    img_array = np.expand_dims(img_array, axis=0)  # Añadir dimensión batch
+    img_array = np.expand_dims(img_array, axis=0)
     return img, img_array
