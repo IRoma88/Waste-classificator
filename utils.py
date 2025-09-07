0f5b89ca9585ba14e5e1fd68c023b093d2ae163f
IRoma88 <ivan.romana88@gmail.com>
2025-09-07 19:30:46 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ from PIL import Image
 
 def load_models():
     # Carga directa de los .h5
-    model_A = tf.keras.models.load_model("modelo_A.h5", compile=False)
-    model_B = tf.keras.models.load_model("modelo_B_finetuned.h5", compile=False)
+    model_A = tf.keras.models.load_model("modelo_A.keras", compile=False)
+    model_B = tf.keras.models.load_model("modelo_B.keras", compile=False)
     return model_A, model_B
 
 def preprocess_image(uploaded_file, target_size):
