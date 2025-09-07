03888a2dd710b8231125ec1f0413de930f48618d
IRoma88 <ivan.romana88@gmail.com>
2025-09-07 19:29:51 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 
 def load_models():
-    # Cargar .h5 directamente
+    # Carga directa de los .h5
     model_A = tf.keras.models.load_model("modelo_A.h5", compile=False)
     model_B = tf.keras.models.load_model("modelo_B_finetuned.h5", compile=False)
     return model_A, model_B
@@ -12,5 +12,5 @@ def preprocess_image(uploaded_file, target_size):
     img = Image.open(uploaded_file).convert("RGB")
     img_resized = img.resize(target_size)
     img_array = tf.keras.utils.img_to_array(img_resized)
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=0)  # Añadir dimensión batch
     return img, img_array
